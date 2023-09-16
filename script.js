69fcc881fb05be92e837efa49474048f0ffa7900
Youssef Eltalyawy <yousefeltalyawy@gmail.com>
2023-09-17 00:21:20 +0300

    Added The four main functions: add, subtract, multiply, and divide

---
+++ script.js
@@ -0,0 +1,23 @@
+function add(firstInt, secondInt) {
+  return firstInt + secondInt;
+}
+
+console.log(add(1,2)) //Now we have the fist function which is the add expected: 3
+
+function subtract(firstInt, secondInt) {
+  return firstInt - secondInt;
+}
+
+console.log(subtract(2,1)); // the subtract function works expected: 1
+
+function multiply(firstInt, secondInt) {
+  return firstInt * secondInt
+}
+
+console.log(multiply(5,2)); // the multiply function works expected: 10
+
+function divide(firstInt, secondInt) {
+  return firstInt / secondInt
+}
+
+console.log(divide(4,2)); // the divide function works expected: 2
