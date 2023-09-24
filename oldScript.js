10ba440fe9e36625049d267db87d9a5124b7b891
Youssef Eltalyawy <yousefeltalyawy@gmail.com>
2023-09-24 14:42:42 +0300

    now the = button works

--- oldScript.js
+++ oldScript.js
@@ -1,11 +1,6 @@
 const calculatorButtons = document.querySelectorAll(".number-button");
 const calculatorDisplay = document.querySelector(".calculator-display");
 
-const plusBtn = document.querySelector("#plus");
-const minusBtn = document.querySelector("#minus");
-const multiplyBtn = document.querySelector("#multiply");
-const divideBtn = document.querySelector("#divide");
-
 const delBtn = document.querySelector("#del");
 const resetBtn = document.querySelector("#reset");
 const equals = document.querySelector("#equals");
@@ -14,12 +9,7 @@ let firstNumber;
 let operator;
 let secondNumber;
 
-let operatorPosition;
-let firstNumberStr = "";
-let secondNumberStr = "";
-
 let displayValue;
-let isOperationInProgress;
 
 function add(firstNumber, secondNumber) {
   return firstNumber + secondNumber;
@@ -62,28 +52,6 @@ calculatorButtons.forEach(function (button) {
   }
 });
 
-plusBtn.addEventListener('click', function() {
-  parseNumbers();
-  secondNumberStr = secondNumberStr.slice(0, -1);
-  pairBehaviour("+");
-});
-minusBtn.addEventListener("click", function () {
-  // parseNumbers();
-  // pairBehaviour("-");
-});
-
-multiplyBtn.addEventListener("click", function () {
-  parseNumbers();
-  secondNumberStr = secondNumberStr.slice(0, -1);
-  pairBehaviour("x");
-});
-
-divideBtn.addEventListener("click", function () {
-  parseNumbers();
-  secondNumberStr = secondNumberStr.slice(0, -1);
-  pairBehaviour("/");
-});
-
 resetBtn.addEventListener("click", function () {
   displayValue = "";
   calculatorDisplay.textContent = displayValue;
@@ -95,20 +63,10 @@ delBtn.addEventListener("click", function () {
 });
 
 equals.addEventListener("click", function () {
-  parseNumbers();
-  displayValue = operate(
-    Number(firstNumberStr),
-    Number(secondNumberStr),
-    operator
-  );
-  console.log(firstNumberStr, secondNumberStr, operator);
-  console.log(displayValue);
-  calculatorDisplay.textContent = displayValue;
-  isOperationInProgress = false;
-  displayValue = calculatorDisplay.textContent;
-});
+  let operatorPosition;
+  let firstNumberStr = "";
+  let secondNumberStr = "";
 
-function parseNumbers() {
   for (let i = 0; i < displayValue.length; i++) {
     const character = displayValue.charAt(i);
     if (
@@ -117,31 +75,20 @@ function parseNumbers() {
       character == "x" ||
       character == "/"
     ) {
+      console.log(character);
       operator = character;
-      operatorPosition = i;
+      operatorPosition = i ;
       break;
     }
   }
   firstNumberStr = displayValue.substring(0, operatorPosition);
+  console.log(firstNumberStr);
   secondNumberStr = displayValue.substring(operatorPosition + 1);
-
-  if(firstNumberStr && secondNumberStr) {
-    console.log(firstNumberStr);
-    console.log(secondNumberStr);
-    isOperationInProgress = true;
-  }
-}
-
-function pairBehaviour() {
-  if(isOperationInProgress) {
-    console.log("busy from function");
-    displayValue = operate(
-      Number(firstNumberStr),
-      Number(secondNumberStr),
-      operator
-    );
-    calculatorDisplay.textContent = displayValue;
-    isOperationInProgress = false;
-    displayValue = calculatorDisplay.textContent;
-  }
-}
+  console.log(secondNumberStr);
+  displayValue = operate(
+    Number(firstNumberStr),
+    Number(secondNumberStr),
+    operator
+  );
+  calculatorDisplay.textContent = displayValue;
+});
